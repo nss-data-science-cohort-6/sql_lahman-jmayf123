297fefa8bbcc949227298f76f83e41d213f0e5af
Jackson Mayfield <jacksonm.tn@gmail.com>
2023-02-11 13:23:44 -0600

    Finished

--- lahman_queries.sql
+++ lahman_queries.sql
@@ -193,14 +193,79 @@ USING(playerid)
 LEFT JOIN managers
 USING(playerid, yearid, lgid)
 LEFT JOIN teams
-USING(yearid, lgid, teamid)
+USING(yearid, lgid, teamid);
 
 -- 7. Which pitcher was the least efficient in 2016 in terms of salary / strikeouts? Only consider pitchers who started at least 10 games (across all teams). Note that pitchers often play for more than one team in a season, so be sure that you are counting all stats for each player.
 
+SELECT  playerid, 
+		SUM(sal.salary) AS total_salary,
+		SUM(pi.so) AS total_strikeouts,
+		SUM(sal.salary) /SUM(pi.so) AS eff
+		
+FROM people AS pe
+INNER JOIN salaries AS sal
+USING(playerid) 
+INNER JOIN pitching AS pi
+USING(playerid, yearid)
+WHERE pi.gs >= 10 AND yearid = 2016
+GROUP BY playerid
+ORDER BY eff DESC;
+
 -- 8. Find all players who have had at least 3000 career hits. Report those players' names, total number of hits, and the year they were inducted into the hall of fame (If they were not inducted into the hall of fame, put a null in that column.) Note that a player being inducted into the hall of fame is indicated by a 'Y' in the **inducted** column of the halloffame table.
+WITH total_hits AS(
+	SELECT playerid, namefirst, namelast, 
+			SUM(h) AS total_hits
+	FROM people AS pe
+	INNER JOIN batting AS bat
+	USING(playerid)
+	GROUP BY playerid
+)
+
+SELECT namefirst, 
+		namelast, 
+		total_hits, 
+		CASE 
+			WHEN inducted = 'Y' THEN yearid
+			WHEN inducted = 'N' THEN NULL
+			ELSE NULL END AS year_inducted
+FROM total_hits
+LEFT JOIN halloffame
+USING(playerid)
+WHERE total_hits >=3000;
 
 -- 9. Find all players who had at least 1,000 hits for two different teams. Report those players' full names.
+WITH total_hits_per_team AS(
+	SELECT playerid, namefirst, namelast, bat.teamid,
+			SUM(h) AS total_hits
+	FROM people AS pe
+	INNER JOIN batting AS bat
+	USING(playerid)
+	GROUP BY playerid, bat.teamid
+)
+
+SELECT namefirst, namelast, total_hits, teamid
+FROM total_hits_per_team
+WHERE playerid IN (
+	SELECT playerid
+	FROM total_hits_per_team
+	WHERE total_hits >= 1000
+	GROUP BY playerid
+	HAVING COUNT(DISTINCT teamid) > 1
+	)
+ORDER BY namefirst;
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
+
+SELECT people.nameFirst, people.nameLast, MAX(Batting.HR) AS HR_2016
+FROM people
+JOIN Batting
+ON people.playerID = Batting.playerID
+WHERE Batting.yearID = 2016
+AND Batting.HR = (SELECT MAX(HR) FROM Batting WHERE playerID = people.playerID)
+AND EXISTS (SELECT 1 FROM Batting WHERE playerID = people.playerID AND yearID >= 2007 AND yearID <= 2016)
+AND Batting.HR > 0
+GROUP BY people.playerID
+ORDER BY HR_2016 DESC;
+
 
 -- After finishing the above questions, here are some open-ended questions to consider.
